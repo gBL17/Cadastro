c3fdcd854218a281fd5f358bb7fedbd642cacbd8
gBL17 <fgrodrigues.gabriel@gmail.com>
2024-02-22 11:07:47 -0300

    (Fix)cadastro_script.php & conexao.php - add button

--- conexao.php
+++ conexao.php
@@ -5,7 +5,13 @@
     $bd = "empresa";
 
     if ($conn = mysqli_connect($server, $user, $pass, $bd)) {
-        echo "Conectado!!";
+        // echo "Conectado!!";
     } else 
         echo "Erro";
+
+    function menssagem($texto, $tipo){
+        echo "<div class='alert alert-$tipo' role='alert'>
+        $texto
+        </div>";
+    }
 ?>
