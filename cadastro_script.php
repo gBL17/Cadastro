133365f54cc6c4e27b7f86b95c8bebc7372fb1c2
gBL17 <fgrodrigues.gabriel@gmail.com>
2024-02-22 10:42:15 -0300

    (Create)cadastro_script.php

---
+++ cadastro_script.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Cadastrado</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="container">
+        <div class="row">
+            <?php
+                include "conexao.php";
+
+                $nome = $_POST['nome'];
+                $endereco = $_POST['endereco'];
+                $telefone = $_POST['telefone'];
+                $email = $_POST['email'];
+                $data_nascimento = $_POST['data_nascimento'];
+
+                $sql = "INSERT INTO `pessoas`(`nome`, `endereco`, `telefone`, `email`, `data_nascimento`) VALUES ('$nome','$endereco','$telefone','$email','$data_nascimento')";
+
+                if (mysqli_query($conn,$sql)) {
+                    echo "$nome foi cadastrado com sucesso!!";
+                } else 
+                    echo "$nome NÃO foi cadastrado";
+            ?>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
