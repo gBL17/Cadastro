4ab27825d27e302f5f7a74a4a75aec582456813f
gBL17 <fgrodrigues.gabriel@gmail.com>
2024-02-22 10:56:01 -0300

    (Fixing)conexao.php commenting echo

--- conexao.php
+++ conexao.php
@@ -5,7 +5,7 @@
     $bd = "empresa";
 
     if ($conn = mysqli_connect($server, $user, $pass, $bd)) {
-        echo "Conectado!!";
+        // echo "Conectado!!";
     } else 
         echo "Erro";
 ?>
